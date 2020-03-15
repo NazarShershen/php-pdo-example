eabcbb9888f75cf1aa46e55fb9fbd9a4f4cff297
Nazar <nazar.shershen@gmail.com>
2020-03-15 17:31:38 +0200

    main tempate for all pages

---
+++ code/views/layout/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Legendary Artifacts Shop</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<?= getHeader() ?>
+
+<? include $pagePath; ?>
+
+</body>
+</html>
