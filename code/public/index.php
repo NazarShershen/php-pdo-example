0cafe06ef3d27429a281442dc8a25fb2daeb3ade
Nazar <nazar.shershen@gmail.com>
2020-03-15 17:34:23 +0200

    include requestes page in index file; show list of goods by default

--- code/public/index.php
+++ code/public/index.php
@@ -3,31 +3,10 @@
 require '../vendor/autoload.php';
 
 use App\Application;
-use App\Shop;
 
 Application::init();
 
-$shop = new Shop();
+$pageName = 'list.php';
+$pagePath = VIEWS_PATH . "/pages/$pageName";
 
-?>
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <title>Legendary Artifacts Shop</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-
-<body>
-
-    <?= getHeader() ?>
-
-    <div class="artifacts-container">
-        <? foreach ($shop->getGoods() as $item) : ?>
-            <? renderCard($item); ?>
-        <? endforeach; ?>
-    </div>
-
-</body>
-</html>
+require_once VIEWS_PATH . '/layout/main.php';
